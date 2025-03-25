8a4dff78a5635e3a9e97f114ed07d51ca41782d6
Libing Chen <libing.chen@gmail.com>
2025-03-25 12:47:25 +0800

    test: PgDataTypeTest

---
+++ src/test/java/org/mvnsearch/dbunit/ext/postgresql/PgDataTypeTest.java
@@ -0,0 +1,35 @@
+package org.mvnsearch.dbunit.ext.postgresql;
+
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.xml.FlatDtdDataSet;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PgDataTypeTest {
+
+    @Test
+    public void testDataTypes() throws Exception {
+        DatabaseConnection connection = new PgConnection(getConnection(), "public");
+        FlatDtdDataSet.write(connection.createDataSet(), Files.newOutputStream(Paths.get("database.dtd")));
+    }
+
+    @Test
+    public void testDataTypes2() throws Exception {
+        JdbcDatabaseTester databaseTester = new JdbcDatabaseTester("org.postgresql.Driver", "jdbc:postgresql://localhost:25432/demo", "postgres", "123456");
+        final IDatabaseConnection connection = databaseTester.getConnection();
+        connection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new PostgresqlExtraDataTypeFactory());
+    }
+
+    public Connection getConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection("jdbc:postgresql://localhost:25432/demo", "postgres", "123456");
+    }
+
+}
